2deb4e90e7215539f51bbc36d229a1d45d79c671
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-04 21:11:22 +0200

    Register models to admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Collection)
+admin.site.register(models.Address)
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
+admin.site.register(models.Customer)
+admin.site.register(models.OrderItem)
+admin.site.register(models.Product)
+admin.site.register(models.Promotion)
