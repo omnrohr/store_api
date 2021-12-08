97661c39ef635a361ed620c5382c6cf7e7e1237f
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-08 17:29:48 +0200

    Registering users in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.models import Product
 from .models import User
 from store.admin import ProductAdmin
 from tags.models import TageedItem
-admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email'),
+        }),
+    )
 
 
 class TagInline(GenericTabularInline):
